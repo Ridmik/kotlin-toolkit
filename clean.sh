9b248f5938425eb51c9dc61befd90d314df4d55c
soumic <soumic@ridmik.com>
2025-08-10 16:50:06 +0600

    🔥 Create jitpack.yml, and clean.sh

---
+++ clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# sometimes android studio builds old apk despite having latest codes. We need to clear the caches
+# to repair this issue
+echo "Cleaning previous builds and caches..."
+./gradlew clean
+
+echo "Cleaning up ~/.gradle/caches/build-cache-1"
+rm -rf ~/.gradle/caches/build-cache-1
+
+# remove build folder if needed
+echo "Force deleting build folders..."
+rm -rf ./build
+rm -rf ./**/build
+
+echo "Force deleting .kotlin folder..."
+rm -rf ./.kotlin
+echo "Force deleting .gradle folder..."
+rm -rf ./.gradle
+rm -rf ./**/.gradle
+
